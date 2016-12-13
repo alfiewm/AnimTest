3123c6079ba31bed1ddbcb6514ea633dc2f69855
wangmeng <wangmeng@fenbi.com>
2016-12-13 16:08:45 +0800

    disable as demo application.

--- app/src/main/java/meng/animtest/DemoApplication.java
+++ app/src/main/java/meng/animtest/DemoApplication.java
@@ -1,13 +1,15 @@
 package meng.animtest;
 
 import android.app.Application;
+<<<<<<< HEAD
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+=======
+>>>>>>> disable as demo application.
 
 import meng.animtest.asdemo.BasicShortcutsDemo;
-import meng.animtest.asdemo.DebuggerDemo;
 
 /**
  * Created by meng on 2016/9/25.
@@ -18,6 +20,7 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
 //        Debug.waitForDebugger();
+<<<<<<< HEAD
         DebuggerDemo.breakOnce(20);
         DebuggerDemo.viewAs(1474778285000L);
         List<Integer> intList = new ArrayList<>();
@@ -27,6 +30,17 @@ public class DemoApplication extends Application {
             DebuggerDemo.conditionalBreakPoint(integer);
         }
         DebuggerDemo.logBreakPoint(324);
+=======
+//        DebuggerDemo.breakOnce(20);
+//        DebuggerDemo.viewAs(1474778285000L);
+//        List<Integer> intList = new ArrayList<>();
+//        intList.addAll(Arrays.asList(new Integer[] {1, 2, 3, 4, 5}));
+//        DebuggerDemo.markObject(intList);
+//        for (Integer integer : intList) {
+//            DebuggerDemo.conditionalBreakPoint(integer);
+//        }
+//        DebuggerDemo.logBreakPoint(324);
+>>>>>>> disable as demo application.
 //        DebuggerDemo.startMainActivity(this);
     }
     
